272c149e14b732b7f29bc7e81d24e5094833a5a1
George <georgie.george7@gmail.com>
2023-08-09 13:45:35 +0400

    fetch data from API

---
+++ src/modules/mazeapi.js
@@ -0,0 +1,19 @@
+const fetchmetaData =  async () => {
+  const url = 'https://api.tvmaze.com/shows';
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const showData = data.map((shows) => ({
+    id: shows.id,
+    name: shows.name,
+    image: shows.image,
+    rating: shows.rating,
+    summary: shows.summary,
+    season: shows.season,
+    airdate: shows.airdate,
+  }));
+
+  return showData;
+};
+
+export default fetchmetaData;

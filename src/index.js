709c0d0f1b9ce0d9a3d119743492dd88d8a8af1a
George <georgie.george7@gmail.com>
2023-08-09 13:46:21 +0400

    update main js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './index.css';
+import fetchmetaData from './modules/mazeapi.js';
+import displayMetaData from './modules/display.js';
+
+const init = async () => {
+  const displayData = await fetchmetaData();
+  displayMetaData(displayData);
+}
+
+init ();

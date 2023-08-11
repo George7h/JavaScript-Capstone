7b79d51197a8b0a3421926a80e38c0189821dd5d
George <georgie.george7@gmail.com>
2023-08-11 13:42:17 +0400

    Add like button and functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import './index.css';
 import fetchmetaData from './modules/mazeapi.js';
 import displayMetaData from './modules/display.js';
+import itemCounter from './modules/itemcounter.js';
+
+const appId = 'XgSataEYPyY1k6p4lDdg';
 
 const init = async () => {
   const displayData = await fetchmetaData();
   displayMetaData(displayData);
-}
+  itemCounter();
 
-init ();
+  // Fetch and update like counts for each show
+  await Promise.all(displayData.map(async (card) => {
+    const likeCountElement = document.getElementById(card.id);
+    const likesResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`);
+    const likesData = await likesResponse.json();
+
+    const showLikes = likesData.find((item) => item.item_id === card.id);
+    if (showLikes) {
+      likeCountElement.textContent = showLikes.likes;
+    }
+  }));
+};
+
+init();
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,3 +1,6 @@
+import postLikeCount from "./postLikeCount.js";
+import { appId } from "./postLikeCount.js";
+
 export default function displayMetaData(displayData) {
   const showSection = document.getElementById('mainContainer');
 
@@ -7,10 +10,16 @@ export default function displayMetaData(displayData) {
 
     const imageElement = document.createElement('img');
     imageElement.src = card.image.medium;
-    
 
     const UI = document.createElement('div');
-    UI.innerHTML = `${card.name}`;
+    UI.innerHTML = `
+      <div class="contain">
+        ${card.name}
+        <span class="material-symbols-outlined thumb-icon" id="thumb_${card.id}">thumb_up</span>
+      </div>
+      Likes: <span id='${card.id}' class="like-count">0</span>
+    `;
+
     const commentsButton = document.createElement('button');
     commentsButton.className = 'comments';
     commentsButton.innerHTML = 'Comments';
@@ -20,5 +29,28 @@ export default function displayMetaData(displayData) {
     div.appendChild(commentsButton);
 
     showSection.appendChild(div);
+
+    // Thumbs-up icon click event
+    const thumbUpIcon = UI.querySelector(`#thumb_${card.id}`);
+    thumbUpIcon.addEventListener('click', async () => {
+      const likeCountElement = UI.querySelector('.like-count');
+      const currentLikes = parseInt(likeCountElement.textContent, 10);
+
+      if (thumbUpIcon.style.color === 'blue') {
+        // Remove like
+        thumbUpIcon.style.color = 'black';
+        likeCountElement.textContent = currentLikes - 1;
+
+        // Call the function to post the updated like count to the API
+        await postLikeCount(appId, card.id, currentLikes - 1);
+      } else {
+        // Add like
+        thumbUpIcon.style.color = 'blue';
+        likeCountElement.textContent = currentLikes + 1;
+
+        // Call the function to post the updated like count to the API
+        await postLikeCount(appId, card.id, currentLikes + 1);
+      }
+    });
   });
 }

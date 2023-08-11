06e8bf61c8792ce9663cc213314b5d258a36653a
George <georgie.george7@gmail.com>
2023-08-11 23:49:03 +0400

    display cards

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import fetchShows from './modules/hitApi.js';
+
 import './index.css';
+
+
+const showContainer = document.getElementById('show-container');
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const shows = await fetchShows();
+  shows.forEach(async (show) => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+      <img src="${show.image.medium}" alt="${show.name}">
+      <p>${show.name}</p>
+      <button class="like-button">Like</button>
+      <div class="like-count">Likes: 0</div>
+    `;
+
+
+    showContainer.appendChild(card);
+  });
+});

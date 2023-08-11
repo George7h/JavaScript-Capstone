14de7baaea49f4aedaa7e5ac7c5bc79834265b2d
George <georgie.george7@gmail.com>
2023-08-11 13:41:34 +0400

    comment out unused code

--- src/modules/Createappid.js
+++ src/modules/Createappid.js
@@ -1,37 +1,36 @@
-async function createapp() {
-  const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+// async function createapp() {
+//   const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
 
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify()
-    });
+//   try {
+//     const response = await fetch(url, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify()
+//     });
 
-    if (response.ok) {
-      const appID = await response.text();
-      console.log('New app created with ID:', appID);
-    } else {
-      console.error('Failed to create new app.');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+//     if (response.ok) {
+//       const appID = await response.text();
+//       console.log('New app created with ID:', appID);
+//     } else {
+//       console.error('Failed to create new app.');
+//     }
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
 
-createapp();
+// createapp();
 
 //App id : XgSataEYPyY1k6p4lDdg
 
-const uniqueAppId = async () => {
-  if (localStorage.getItem('uniqueAppId')) {
-    return localStorage.getItem('uniqueAppId');
-  }
-  const newAppId = await createApp();
-  return newAppId;
-};
+// const uniqueAppId = async () => {
+//   if (localStorage.getItem('uniqueAppId')) {
+//     return localStorage.getItem('uniqueAppId');
+//   }
+//   const newAppId = await createApp();
+//   return newAppId;
+// };
 
-export default uniqueAppId;
-
+// export default uniqueAppId;
